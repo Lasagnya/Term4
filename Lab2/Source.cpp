189d854ba16ed9379bacd54e55939cfb86a28ded
Lasagnya <stepik-2003@yandex.ru>
2022-03-04 23:06:20 +0300

    do random input

--- Lab2/Source.cpp
+++ Lab2/Source.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <process.h>
 #include <conio.h>
@@ -6,15 +7,26 @@
 using namespace std;
 
 int main() {
-	int n = 5;
+	char* lpszCommandLine = new char[255];
+	lpszCommandLine[0] = '\0';
+	strcat(lpszCommandLine, "Lab2_Child ");
+	int n = rand()%10;
+	char* temp = new char[255];
+	_itoa(n, temp, 10);
+	strcat(lpszCommandLine, strcat(temp, " "));
 	double* array = new double[n];
 	for (int i = 0; i < n; i++) {
 		array[i] = rand() % 10;
+		sprintf(temp, "%f", array[i]);
+		strcat(lpszCommandLine, strcat(temp, " "));
 		cout << array[i] << " ";
 	}
 	cout << "\n";
-	int K = 4;
-	char lpszCommandLine[] = "Lab2_Child 5 1.0 2.0 3.0 4.0 5.0 4.exe";
+	int K = rand() % n;
+	_itoa(K, temp, 10);
+	strcat(lpszCommandLine, temp);
+	strcat(lpszCommandLine, ".exe");
+	//char lpszCommandLine[] = "Lab2_Child 5 1.0 2.0 3.0 4.0 5.0 4.exe";
 	STARTUPINFO si;
 	PROCESS_INFORMATION piApp;
 	ZeroMemory(&si, sizeof(STARTUPINFO));
